d7f97103d4ef64a85b3fb9b81ba3621e5b230be9
Rowan Hill <rowan.hill@softwire.com>
2013-12-26 22:43:04 +0000

    Remove WireMock standalone jar from repo, download it automatically

--- wiremock/start.sh
+++ wiremock/start.sh
@@ -6,12 +6,23 @@ cd ../wiremock
 
 # Ensure WireMock isn't already running
 if [ -e wiremock.pid ]; then
-    echo WireMock is already started: see process `cat wiremock.pid`
+    echo WireMock is already started: see process `cat wiremock.pid` 1&>2
     exit 1
 fi
 
+# Download the wiremock jar if we need it
+if ! [ -e wiremock-standalone.jar ]; then
+    echo WireMock standalone JAR missing. Downloading.
+    curl http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock/1.39/wiremock-1.39-standalone.jar -o wiremock-standalone.jar
+    status = $?
+    if [ $status -ne 0 ]; then
+        echo curl could not download WireMock JAR 1&>2
+        exit $status
+    fi
+fi
+
 # Start WireMock in standalone mode (in a background process) and save its output to a log
-java -jar wiremock-1.33-standalone.jar &> wiremock.log 2>&1 &
+java -jar wiremock-standalone.jar &> wiremock.log 2>&1 &
 echo $! > wiremock.pid
 
 echo WireMock started
--- wiremock/stop.sh
+++ wiremock/stop.sh
@@ -8,7 +8,7 @@ if [ -e wiremock.pid ]; then
   kill -9 `cat wiremock.pid`
   rm wiremock.pid
 else
-  echo WireMock is not started
+  echo WireMock is not started 1&>2
   exit 1
 fi
 
