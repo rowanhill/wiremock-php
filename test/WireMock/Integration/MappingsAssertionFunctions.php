0290e75c481a01f87a5fd957d047439756ae28da
Rowan Hill <rowan.hill@gmail.com>
2022-06-12 13:50:21 +0300

    Hackily use assertEquals in mapping assertions

--- test/WireMock/Integration/MappingsAssertionFunctions.php
+++ test/WireMock/Integration/MappingsAssertionFunctions.php
@@ -1,5 +1,6 @@
 <?php
 
+use PHPUnit\Framework\Assert;
 use WireMock\Client\ListStubMappingsResult;
 use WireMock\Serde\SerializerFactory;
 use WireMock\Stubbing\StubMapping;
@@ -12,9 +13,22 @@ function assertThatTheOnlyMappingPresentIs(StubMapping $localStubMapping)
     $mappingsFromServer = getMappings();
     assertThat($mappingsFromServer, is(arrayWithSize(1)));
 
+    /*
+     * assertEquals gives good diffing of differences, so we want to use it as our assertion, but does considers
+     * empty arrays not equal to null.
+     *
+     * We want to treat empty arrays as equal to null, as the local stub mapping will frequently have empty
+     * array values, which will be dropped in serializing (both in data to and from WireMock), and will show
+     * as null in the version returned from the server.
+     *
+     * As a compromise (aka "hack"), we first check the non-strict equality with PHP's != operator. This *does* consider
+     * empty arrays and null to be the same. Only if this test fails do we use assertEquals (to avoid false failures).
+     *
+     * As an extra safety check, we also use Hamcrest's equalTo, which considers empty arrays equal to null.
+     */
     /** @noinspection PhpNonStrictObjectEqualityInspection */
     if ($mappingsFromServer[0] != $localStubMapping) {
-        var_dump('Server', $mappingsFromServer[0], 'Local', $localStubMapping);
+        Assert::assertEquals($localStubMapping, $mappingsFromServer[0]);
     }
     assertThat($mappingsFromServer[0], equalTo($localStubMapping));
 }
